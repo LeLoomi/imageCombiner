ef91aecc0aac4ea8b5498f7fe3983ffea16bccaf
LeLoomi <lohr.eliah@gmail.com>
2022-01-22 12:45:18 +0100

    added basic mode selection, functionality to create all possible combinations, "skip compensation" in random mode and isolated stat counters from actual blue collar code to prevent interference
    Signed-off-by: LeLoomi <lohr.eliah@gmail.com>

--- main.py
+++ main.py
@@ -2,61 +2,159 @@ import os
 from numpy.random import randint
 from PIL import Image
 
+# gen parameters
+variations = 5
+layers = 4  # before changing, adapt coms+layers below
 
-#gen parameters
-variations = 6          #+1 to the actual files you have whyever idk dont want to look at it
-images_count = 100     #final count, set to -1 to generate all
-layers = 4              #before changing adapt coms+layers below
-
-
-#internal vars
+# internal counting
+order_count = 0
 skipped = 0
-generated = 0
 
-if images_count == -1:
-    images_count = pow(variations - 1, layers)
-digits = sum(c.isdigit() for c in str(images_count))    #get count digits for clean output
+order_count_stat = 0
+iterations_stat = 0
+generated_stat = 0
+skipped_stat = 0
 
-#create the filename by rolling a random number etc
-def generate_filename():
+
+# create the filename by rolling a random number etc
+def generate_filename_random():
     name = ""
-    for int in range(layers):
-        name = name + str(randint(1, variations))
+    for i in range(layers):
+        name = name + str(randint(1, variations + 1))
     return name
 
-#list that contains all the names
-files = []
-existing = os.listdir("./output/")
+
+# list that contains all the names
+files_generated = []
+existing_files = os.listdir("./output/")
 
 
-for i in range(images_count):
-    name = generate_filename()
+def generate_complete_collection():
+    # to tell user the no. of image in console etc.
+    progress = 0
+    global generated_stat
+    global skipped_stat
+    global iterations_stat
 
-    if name not in files and (name + ".png") not in existing:
-        files.append(name)
+    # count the digits for clean progression output
+    digits = sum(c.isdigit() for c in str(order_count))
 
-        for item in files:
-            layer1 = Image.open(f'./layer1/{item[0]}.png').convert('RGBA')
-            layer2 = Image.open(f'./layer2/{item[1]}.png').convert('RGBA')
-            layer3 = Image.open(f'./layer3/{item[2]}.png').convert('RGBA')
-            layer4 = Image.open(f'./layer4/{item[3]}.png').convert('RGBA')
+    for l1 in range(variations):
+        for l2 in range(variations):
+            for l3 in range(variations):
+                for l4 in range(variations):
+                    progress += 1
+                    iterations_stat += 1
 
-            com1 = Image.alpha_composite(layer1, layer2)
-            com2 = Image.alpha_composite(com1, layer3)
-            com3 = Image.alpha_composite(com2, layer4)
+                    name = str(l1 + 1) + str(l2 + 1) + str(l3 + 1) + str(l4 + 1)  # assemble filename from the fors
+                    if name not in files_generated and (
+                            name + ".png") not in existing_files:  # check if file already exists (to skip)
+                        files_generated.append(name)
 
-            rgb_im = com3.convert('RGB')
-            rgb_im.save("./output/" + item + ".png")
+                        for item in files_generated:
+                            layer1 = Image.open(f'./layer1/{item[0]}.png').convert('RGBA')
+                            layer2 = Image.open(f'./layer2/{item[1]}.png').convert('RGBA')
+                            layer3 = Image.open(f'./layer3/{item[2]}.png').convert('RGBA')
+                            layer4 = Image.open(f'./layer4/{item[3]}.png').convert('RGBA')
 
-        generated = generated + 1
-        print("[" + str(name) + " | " + str(i+1).zfill(digits) + "/" + str(images_count) + "] -> generated")
+                            com1 = Image.alpha_composite(layer1, layer2)
+                            com2 = Image.alpha_composite(com1, layer3)
+                            com3 = Image.alpha_composite(com2, layer4)
+
+                            rgb_im = com3.convert('RGB')
+                            rgb_im.save("./output/" + item + ".png")
+
+                        generated_stat += 1
+                        print("[" + str(name) + " | " + str(progress).zfill(digits) + "/" + str(
+                            order_count) + "] -> generated")
+
+                    else:
+                        skipped_stat += 1
+
+                        print("[" + str(name) + " | " + str(progress).zfill(digits) + "/" + str(
+                            order_count) + "] -> skipped")
+
+
+def generate_random_collection():
+    global skipped
+    global skipped_stat
+    global order_count
+    global generated_stat
+    global iterations_stat
+
+    # to tell user the no. of image in console etc.
+    progress_stat = 0
+    # count the digits for clean progression output
+    digits = sum(c.isdigit() for c in str(order_count))
+
+    for i in range(order_count):
+        progress_stat += 1
+        iterations_stat += 1
+
+        name = generate_filename_random()
+
+        if name not in files_generated and (name + ".png") not in existing_files:
+            files_generated.append(name)
+
+            for item in files_generated:
+                layer1 = Image.open(f'./layer1/{item[0]}.png').convert('RGBA')
+                layer2 = Image.open(f'./layer2/{item[1]}.png').convert('RGBA')
+                layer3 = Image.open(f'./layer3/{item[2]}.png').convert('RGBA')
+                layer4 = Image.open(f'./layer4/{item[3]}.png').convert('RGBA')
+
+                com1 = Image.alpha_composite(layer1, layer2)
+                com2 = Image.alpha_composite(com1, layer3)
+                com3 = Image.alpha_composite(com2, layer4)
+
+                rgb_im = com3.convert('RGB')
+                rgb_im.save("./output/" + item + ".png")
+
+            print("[" + str(name) + " | " + str(progress_stat).zfill(digits) + "/" + str(order_count) + "] -> generated")
+            generated_stat += 1
+
+        else:
+            skipped += 1
+            skipped_stat += 1
+            print("[" + str(name) + " | " + str(progress_stat).zfill(digits) + "/" + str(order_count) + "] -> skipped")
+
+            # to create another one after this run since we'd be 1 down every skip otherwise
+            order_count += 1
+            continue
+
+    # to create the "missing" ones to actually gen 10 if 10 ordered by user
+    if 0 < skipped <= pow(variations, layers):
+            order_count = skipped
+            skipped = 0
+
+            generate_random_collection()
+
+
+print("---------------------------"
+      "\nSet up for", variations, "variations &", layers, "layers"
+      "\n\nSelect to continue:"
+      "\n 1: Create all possible combinations"
+      "\n 2: Create a random collection"
+      "\n 3: Exit program")
+
+while True:
+    mode = input("Selection » ")
+
+    if mode == "1":
+        order_count = pow(variations, layers)
+        generate_complete_collection()
+        break
+    elif mode == "2":
+        order_count = int(input("Count of images to generate: "))
+        generate_random_collection()
+        break
+    elif mode == "3":
+        raise SystemExit(0)
     else:
-        skipped = skipped + 1
-        print("[" + str(name) + " | " + str(i+1).zfill(digits) + "/" + str(images_count) + "] -> skipped")
+        print("\u26A0 Faulty input")
         continue
 
 print("---------------------------"
-      "\nFinal stats:"
-      "\nOrdered:   ", images_count,
-      "\nGenerated: ", generated,
-      "\nSkipped:   ", skipped)
+      "\nFinal statistics:"
+      "\nIterations: ", iterations_stat,
+      "\nGenerated:  ", generated_stat,
+      "\nSkipped:    ", skipped_stat)
